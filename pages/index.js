13594341a37e89d4168d2121a9ce7ad15bcc4554
abuzahid <abuzahidonline@gmail.com>
2022-06-25 01:49:10 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Md. Abu Zahid" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Computer vision and machine learning professional
         </p>
       </main>
 
